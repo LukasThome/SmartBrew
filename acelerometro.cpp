c955cda139464446673bf893dea82038d762a36e
Lucas Thome da Silva <30710328+LukasThome@users.noreply.github.com>
2017-10-08 19:42:01 -0300

    Add files via upload

---
+++ acelerometro.cpp
@@ -0,0 +1,34 @@
+#includue <stdio.h>
+
+int main () {
+	
+	float mediaVetor = 0;
+	float somaVetor = 0;
+	float eixoX[20] = {};
+	int i;
+	
+	//Guarda as 20 leituras do eixo x em um vetor
+	for (i = 0; i < 20; i++)
+	{
+
+	eixoX[i] = 	analogRead(X);
+	
+	
+	}
+	//Soma as 20 leituras do eixo X
+	for (i = 0; i < 20; i++){
+		
+		somaVetor = eixoX[i] + somaVetor;
+		
+	}
+
+	//Faz a media das 20 leituras do eixo x
+	for(i = 0; i < 20; i++){
+		
+		mediaVetor = somaVetor/20;
+	}
+
+
+}
+
+
