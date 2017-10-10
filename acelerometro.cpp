da27b021192559d807cecbcac1e0aac04d5a2ffc
Lucas Thome da Silva <30710328+LukasThome@users.noreply.github.com>
2017-10-09 22:46:23 -0300

    Update acelerometro.cpp

--- acelerometro.cpp
+++ acelerometro.cpp
@@ -6,19 +6,59 @@ int main ()
 	float mediaVetor = 0;
 	float somaVetor = 0;
 	float eixoX[20] = {};
+	float eixoY[20] = {};
+	float eixoZ[20] = {};
 	int i;
+	float eixo;
+	float leituraInicial = 0;
+	float leituraAtual = 0;
+	float variacao = 0;
 	
-	//Guarda as 20 leituras do eixo x em um vetor
-	for (i = 0; i < 20; i++)
+	
+	//Função que retornará a diferença de duas leituras de um eixo qualquer escolhido
+	float variacaoEixo(float eixo)
 	{
-	eixoX[i] = analogRead(X);
+		//Guarda as 20 leituras do eixo x em um vetor
+		for (i = 0; i < 20; i++)
+		{
+		eixoX[i] = analogRead(X);
+		}
+		//Soma as 20 leituras do eixo X
+		for (i = 0; i < 20; i++){
+		somaVetor = eixoX[i] + somaVetor;	
+		}
+		//Faz a media das 20 leituras do eixo 	
+		mediaVetor = somaVetor/20;
+		
+		mediaVetor = valorInicial;
+		
+		delay(1000);
+		
+		//Guarda as 20 leituras do eixo x em um vetor
+		for (i = 0; i < 20; i++)
+		{
+		eixoX[i] = analogRead(X);
+		}
+		//Soma as 20 leituras do eixo X
+		for (i = 0; i < 20; i++){
+		somaVetor = eixoX[i] + somaVetor;	
+		}
+		//Faz a media das 20 leituras do eixo 	
+		mediaVetor = somaVetor/20;
+		
+		mediaVetor = valorAtual;
+		
+		variacao = valorAtual - ValorInicial;
+		
+		return(variacao);
+		
+	
 	}
-	//Soma as 20 leituras do eixo X
-	for (i = 0; i < 20; i++){
-	somaVetor = eixoX[i] + somaVetor;	
-	}
-	//Faz a media das 20 leituras do eixo 	
-	mediaVetor = somaVetor/20;
+
+//Verifica se os demais eixos estão não estão variando, impedindo que ocorra uma leitura errada da densidade caso alguem balance ou 
+//toque no recipiente do mosto
+	
+
 	
 
 
